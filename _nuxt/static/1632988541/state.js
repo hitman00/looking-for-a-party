4e33514bf319729c89042ac98c7879ea93ce610b
hitman00 <hitman00@users.noreply.github.com>
2021-09-30 08:06:42 +0000

    deploy: de67784e2c304256074bf115c21775cc06478b75

--- _nuxt/static/1632988541/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Flooking-for-a-party\u002F_nuxt\u002Fstatic\u002F1632988541",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Flooking-for-a-party\u002F",assetsPath:"\u002Flooking-for-a-party\u002F_nuxt\u002F",cdnURL:a}}}}(null));
